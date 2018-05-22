c00933215140faa1c4afb93a954d6c61084dae15
homahi <homahi0128@gmail.com>
2018-05-23 06:38:29 +0900

    Initial Commit

---
+++ AVPlayer_ProtoTests/AVPlayer_ProtoTests.swift
@@ -0,0 +1,36 @@
+//
+//  AVPlayer_ProtoTests.swift
+//  AVPlayer_ProtoTests
+//
+//  Created by 原野誉大 on 2018/05/23.
+//  Copyright © 2018年 原野誉大. All rights reserved.
+//
+
+import XCTest
+@testable import AVPlayer_Proto
+
+class AVPlayer_ProtoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
