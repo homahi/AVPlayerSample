9aa286ba908e6d665f0e1bee25564697b9261035
homahi <homahi0128@gmail.com>
2018-05-23 07:29:02 +0900

    AVPlayerSample

--- AVPlayer_Proto/ViewController.swift
+++ AVPlayer_Proto/ViewController.swift
@@ -7,19 +7,97 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var videoVIew: UIView!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    var player: AVPlayer!
+    var playerLayer: AVPlayerLayer!
+    
+    var isVideoPlaying = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8")!
+        player = AVPlayer(url: url)
+        player.currentItem?.addObserver(self, forKeyPath: "duration", options: [.new, .initial], context: nil)
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = .resize
+        
+        videoVIew.layer.addSublayer(playerLayer)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playerLayer.frame = videoVIew.bounds
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        player.play()
+    }
 
 
+    @IBAction func playPressed(_ sender: UIButton) {
+        if isVideoPlaying {
+            player.pause()
+            sender.setTitle("Pause", for: .normal)
+        } else {
+            player.play()
+            sender.setTitle("Play", for: .normal)
+        }
+            isVideoPlaying = !isVideoPlaying
+    }
+    @IBAction func forwardPressed(_ sender: UIButton) {
+        guard let duration = player.currentItem?.duration else { return }
+        let currentTime = CMTimeGetSeconds(player.currentTime())
+        let newTime = currentTime + 5.0
+        
+        if newTime < CMTimeGetSeconds(duration) - 5.0 {
+            let time: CMTime = CMTimeMake(Int64(newTime*1000), 1000)
+            player.seek(to: time)
+        }
+    }
+    @IBAction func backwardsPressed(_ sender: UIButton) {
+        let currentTime = CMTimeGetSeconds(player.currentTime())
+        var newTime = currentTime - 5.0
+        
+        if newTime < 0 {
+            newTime = 0
+        }
+        let time: CMTime = CMTimeMake(Int64(newTime*1000), 1000)
+        player.seek(to: time)
+    }
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "duration", let duration = player.currentItem?.duration.seconds, duration > 0.0 {
+            
+        }
+    }
+    
+    func getTimeString(from time: CMTime) -> String {
+        let totalSeconds = CMTimeGetSeconds(time)
+        let hours = Int(totalSeconds/3600)
+        let minutes = Int(totalSeconds/60) % 60
+        let seconds = Int(totalSeconds.truncatingRemainder(dividingBy: 60))
+        if hours > 0 {
+            return String(format: "%i:%02i:%02i", arguments: [hours, minutes, seconds] )
+        } else {
+            return String(format: "%02i:%02i", arguments: [minutes, seconds] )
+        }
+    }
 }
 
